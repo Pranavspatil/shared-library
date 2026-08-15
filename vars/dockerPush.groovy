74c23a6f8d3a137c7c3bee7f58cffb2a61e318b9
Pranavspatil <113474526+Pranavspatil@users.noreply.github.com>
2026-08-15 20:32:44 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,3 +1,9 @@
-def call(String dockerHubUser, String projectname , String tag){
-  sh """ docker push dockerHubUser/projectname:tag"""
+def call(String dockerHubUser, String imageName , String tag){
+  def image = "${dockerHubUser}/${imageName}:${tag}"
+
+    echo "Pushing Docker image: ${image}"
+
+    sh """
+        docker push ${image}
+    """
 }
